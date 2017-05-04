1cd3a33a161c3327000f0f3f2117b0344a5ba62a
Cary Jiang <cary.hejiang@gmail.com>
2017-05-04 16:26:20 -0500

    add more source code for tools

---
+++ graph/README.txt
@@ -0,0 +1,8 @@
+Correspondance of Postscript files/dot graphs to Figures in report
+Figure 1: linux_260_sec.ps
+Figure 2: linux_41013_sec.ps
+Figure 3: linux_41013_sec_selinux.ps
+Figure 4: apparmor.ps
+Figure 5: tomayo.ps
+Figure 6: smack.ps
+Figure 7: yama.ps
--- tools/WordCount.java
+++ tools/WordCount.java
@@ -34,12 +34,6 @@ public class WordCount {
 
     protected static List<Tuple2<String, Integer>> wordCount(final JavaRDD<String> textFile) {
 
-        // TODO alter the map/reduce operation on textFile so that only words of length 5 are counted
-        // The length of a string s can be found by using the method s.length()
-        // RDDs have a method filter that takes a boolean test and returns a new RDD that contains
-        // only the elements for which the testing function passed.
-        // The elements of a pair, t, can be retrieved using the accessors t._1 and t._2.
-
         final JavaPairRDD<String, Integer> counter = textFile
             .flatMap(s -> Arrays.asList(s.split(" ")))
             .filter(s -> (s.equalsIgnoreCase("security") || s.equalsIgnoreCase("secure")))
